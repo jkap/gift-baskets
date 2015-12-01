b7c329f6d04578121b99abd25c4bf62392990388
Jenn Kaplan <jenn.kaplan@ave81.com>
2015-12-01 15:06:56 -0600

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to gift-baskets
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/gift_baskets/core_test.clj
@@ -0,0 +1,7 @@
+(ns gift-baskets.core-test
+  (:require [clojure.test :refer :all]
+            [gift-baskets.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
