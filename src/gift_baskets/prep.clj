728375df89ba12a89918f0ccd86c6ebde429a411
Jenn Kaplan <jenn.kaplan@ave81.com>
2015-12-01 15:42:15 -0600

    Added some prep scaffolding

--- src/gift_baskets/prep.clj
+++ src/gift_baskets/prep.clj
@@ -1,4 +1,13 @@
 (ns gift-baskets.prep
+  (:require [clj-http.client :as client])
   (:gen-class))
 
-(defn get-categories [] (println "getting categories"))
+(def products-list-url "http://www.winecountrygiftbaskets.com/gift_basket_list_ajax.asp?RST=1&RSV=171&RFV=&prfrom=&prto=&rc=")
+
+(defn get-product-list []
+  (println "getting products")
+  (client/get ))
+
+(defn- get-cached-product-list [])
+
+(defn- cache-product-list [])
