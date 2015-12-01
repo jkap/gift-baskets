0487fa2890499d0cc39b093e5288ebfa3ed39fed
Jenn Kaplan <jenn.kaplan@ave81.com>
2015-12-01 15:22:58 -0600

    Initial scaffolding work

--- src/gift_baskets/core.clj
+++ src/gift_baskets/core.clj
@@ -1,7 +1,14 @@
 (ns gift-baskets.core
+  (:require [clojure.tools.cli :refer [parse-opts]]
+            [gift-baskets.prep :as prep])
   (:gen-class))
+
+(def cli-options [])
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (def opts (parse-opts args cli-options))
+  (if (= (first (:arguments opts)) "prep") (do
+    (println "prepping")
+    (prep/get-categories))))
---
+++ src/gift_baskets/prep.clj
@@ -0,0 +1,4 @@
+(ns gift-baskets.prep
+  (:gen-class))
+
+(defn get-categories [] (println "getting categories"))
